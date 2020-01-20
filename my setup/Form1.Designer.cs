959c1b3d628d6aef2188f419411c4c4bde66a260
stupendoo <59236268+stupendoo@users.noreply.github.com>
2020-01-20 19:05:39 +0100

    gfdchgchj

--- my setup/Form1.Designer.cs
+++ my setup/Form1.Designer.cs
@@ -31,17 +31,16 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button2 = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.button1.Location = new System.Drawing.Point(223, 117);
+            this.button1.Location = new System.Drawing.Point(416, 406);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(226, 61);
             this.button1.TabIndex = 0;
@@ -51,7 +50,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(223, 247);
+            this.textBox1.Location = new System.Drawing.Point(407, 484);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(219, 22);
             this.textBox1.TabIndex = 1;
@@ -66,63 +65,51 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "label1";
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(64, 33);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(519, 213);
+            this.dataGridView1.TabIndex = 3;
+            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(512, 154);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.button2.Location = new System.Drawing.Point(416, 303);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(117, 115);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
+            this.button2.Size = new System.Drawing.Size(226, 61);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "OK";
             this.button2.UseVisualStyleBackColor = true;
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel1.Location = new System.Drawing.Point(362, 303);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(169, 222);
-            this.panel1.TabIndex = 4;
-            // 
-            // panel2
-            // 
-            this.panel2.Location = new System.Drawing.Point(330, 47);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 100);
-            this.panel2.TabIndex = 5;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(3, 216);
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.button3.Location = new System.Drawing.Point(31, 428);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(145, 73);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "button3";
+            this.button3.Size = new System.Drawing.Size(226, 61);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "OK";
             this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(570, 95);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 7;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(654, 555);
-            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -131,13 +118,11 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
+        public System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
     }
 }
 
--- my setup/Form1.cs
+++ my setup/Form1.cs
@@ -18,7 +18,44 @@ namespace my_setup
 
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Text = textBox1.Text;
+            Form2 f = new Form2();
+            f.label1.Text = label1.Text;
+
+            if (dataGridView1.SelectedCells.Count > 0)
+            {
+                f.textBox1.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                f.textBox2.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                f.ShowDialog();
+
+                
+            }
+
+
+
+
+
+
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dabaDataContext db = new dabaDataContext();
+
+            var f = db.nbChauffeur();
+
+            label1.DataBindings.Add("text",f, "nb");
+
+            var k = from a in db.Chauffeur select new { a.cinChauffeur, a.nomChauffeur };
+            dataGridView1.DataSource = k;
+           
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            f.ShowDialog();
         }
     }
 }

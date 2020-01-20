8a864b7e968f35b36d7f56223ff065cc97ae67ec
stupendoo <59236268+stupendoo@users.noreply.github.com>
2020-01-20 19:10:49 +0100

    45646

--- my setup/Form1.Designer.cs
+++ my setup/Form1.Designer.cs
@@ -40,7 +40,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.button1.Location = new System.Drawing.Point(416, 406);
+            this.button1.Location = new System.Drawing.Point(209, 365);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(226, 61);
             this.button1.TabIndex = 0;
@@ -50,7 +50,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(407, 484);
+            this.textBox1.Location = new System.Drawing.Point(216, 467);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(219, 22);
             this.textBox1.TabIndex = 1;
@@ -59,7 +59,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.label1.Location = new System.Drawing.Point(268, 352);
+            this.label1.Location = new System.Drawing.Point(278, 282);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(79, 29);
             this.label1.TabIndex = 2;
@@ -77,7 +77,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.button2.Location = new System.Drawing.Point(416, 303);
+            this.button2.Location = new System.Drawing.Point(391, 282);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(226, 61);
             this.button2.TabIndex = 4;
@@ -88,7 +88,7 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.button3.Location = new System.Drawing.Point(31, 428);
+            this.button3.Location = new System.Drawing.Point(23, 282);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(226, 61);
             this.button3.TabIndex = 5;
@@ -99,7 +99,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(654, 555);
+            this.ClientSize = new System.Drawing.Size(669, 565);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.dataGridView1);
